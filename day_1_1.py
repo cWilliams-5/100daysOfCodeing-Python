4eac6b381e0e47ea1d93e847cf388ceb979d4af7
Clayton Williams <109181719+cWilliams-5@users.noreply.github.com>
2023-12-12 21:30:37 -0700

    Delete day_1_1.py

--- day_1_1.py
+++
@@ -1,9 +0,0 @@
-# Band Name Generator
-# 100 Days Of Python Day 1
-
-print("Welcom to the Band Name Generator.")
-street = input("What's the name of the "
-               "city you grew up in?\n")
-pet = input("What's your pet's name?\n")
-print("Your band name could be " +
-      street + " " + pet)
